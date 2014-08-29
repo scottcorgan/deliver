d352d190117548f419196d999d46204c42bf56ea
Scott Corgan <scottcorgan@gmail.com>
2014-08-29 12:51:11 -0700

    deps: send 0.8.x

--- index.js
+++ index.js
@@ -14,15 +14,8 @@ var defaultOptions = {
 var deliver = function (req) {
   var options = defaults(arguments[1], defaultOptions);
   
-  console.log('TEST: req.url: ' + req.url);
-  console.log('TEST: options', options);
-  
   // Remote
   if (isUrl(options.root)) {
-    
-    console.log('TEST: is url');
-    console.log('TEST: url proxy request: ' + urlJoin(options.root, req.url));
-    
     delete req.headers.host;
     
     return request(urlJoin(options.root, req.url), {
